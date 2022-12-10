5d3331e74919bbd8d6b9cff575447db44052fb70
Arthur-DevionFox <blackberserkfox@mail.ru>
2022-12-10 21:17:26 +0300

    Начать JS, почти доделать popup

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+/*todo CREATE CODE FOR EDIT BUTTON*/
+
+/*const popupLink = document.querySelectorAll('.popup-link');
+const body = document.querySelector('.body');
+
+if (popupLink.length > 0) {
+    for (let count = 0; count < popupLink.length; count++) {
+        const popLink = popupLink[count];
+        popLink.addEventListener('click', function (add){
+            const popClass = popLink.getAttribute('href').replace('#', '');
+
+        })
+    }
+}*/
+
