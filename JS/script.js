e42257dcb2b6fc38f6abef120a5d6cd2e4a89fdd
Arthur-DevionFox <blackberserkfox@mail.ru>
2022-12-11 01:46:47 +0300

    Почти закончить JS

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,30 @@
-/*todo CREATE CODE FOR EDIT BUTTON*/
+const popup = document.querySelector('.popup');
+const edit = document.querySelector('.profile__edit-button');
+const close = document.querySelector('.popup__close');
+const save = document.querySelector('.popup__save');
+const name = document.querySelector('.profile__name')
+const nameRes = document.querySelector('.popup__name[type=text]')
+const profession = document.querySelector('.profile__profession')
+const professionRes = document.querySelector('.popup__profession[type=text]')
 
-/*const popupLink = document.querySelectorAll('.popup-link');
-const body = document.querySelector('.body');
 
-if (popupLink.length > 0) {
-    for (let count = 0; count < popupLink.length; count++) {
-        const popLink = popupLink[count];
-        popLink.addEventListener('click', function (add){
-            const popClass = popLink.getAttribute('href').replace('#', '');
+function openPopUp() {
+    popup.classList.add('popup__opened');
+}
+function closePopUp() {
+    popup.classList.remove('popup__opened');
+}
 
-        })
-    }
-}*/
+function saveFunc() {
+    name.textContent = nameRes.value
+    profession.textContent = professionRes.value
+}
 
+    edit.addEventListener('click', openPopUp);
+
+    close.addEventListener('click', closePopUp);
+
+    save.addEventListener('click', closePopUp);
+/*    save.addEventListener('', closePopUp);*/
+
+    save.addEventListener('click', saveFunc);
