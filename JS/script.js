6a859db564b5b3975991eb3132c94f4645d957c2
Arthur-DevionFox <blackberserkfox@mail.ru>
2022-12-17 23:41:32 +0300

    Сделать правки по ревью

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,31 @@
 const popup = document.querySelector('.popup');
 const edit = document.querySelector('.profile__edit-button');
 const close = document.querySelector('.popup__close');
-const save = document.querySelector('.popup__save');
+const form = document.querySelector('.popup__form');
 const name = document.querySelector('.profile__name')
-const nameRes = document.querySelector('.popup__info_name')
+const nameRes = document.querySelector('.popup__info_type_name[type=text]')
 const profession = document.querySelector('.profile__profession')
-const professionRes = document.querySelector('.popup__info_profession')
+const professionRes = document.querySelector('.popup__info_type_profession[type=text]')
 
 function popUpOpn() {
-        popup.classList.add('popup_opened');
-        nameRes.value = name.textContent
-        professionRes.value = profession.textContent
+    popup.classList.add('popup_opened');
+    nameRes.value = name.textContent
+    professionRes.value = profession.textContent
 }
 
 function popUpCls(evt) {
-        popup.classList.remove('popup_opened');
-        evt.preventDefault();
+    popup.classList.remove('popup_opened');
+    evt.preventDefault();
 }
 
 function saveFunc(evt) {
-    popup.classList.remove('popup_opened')
+    popUpCls(evt)
     name.textContent = nameRes.value
     profession.textContent = professionRes.value
     evt.preventDefault();
 }
-
 edit.addEventListener('click', popUpOpn);
 close.addEventListener('click', popUpCls);
-
-
-/*TODO CREATE CALL TO SAVE ALL*/
-save.addEventListener('click', saveFunc);
+form.addEventListener('submit', saveFunc);
 
 
