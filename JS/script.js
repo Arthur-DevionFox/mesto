cc59fcdf03344657fd78f9979791a846c1c48e49
Arthur-DevionFox <blackberserkfox@mail.ru>
2023-02-05 15:05:48 +0300

    .

--- JS/script.js
+++ JS/script.js
@@ -13,15 +13,15 @@ const formEdit = document.querySelector('#form-edit');
 const formAdd = document.querySelector('#form-add');
 /**/
 const nameProf = document.querySelector('.profile__name')
-const nameRes = document.querySelector('.popup__input_type-name');
+const nameRes = document.querySelector('.popup__input_type_name');
 /**/
 const profession = document.querySelector('.profile__profession');
-const professionRes = document.querySelector('.popup__input_type-profession');
+const professionRes = document.querySelector('.popup__input_type_profession');
 /**/
 const img = document.querySelector('.popup__image');
 const txt = document.querySelector('.popup__undertaker');
-const headInput = popupAdd.querySelector('.popup__input_type-head');
-const urlInput = popupAdd.querySelector('.popup__input_type-url');
+const headInput = popupAdd.querySelector('.popup__input_type_head');
+const urlInput = popupAdd.querySelector('.popup__input_type_url');
 
 /*Карточки*/
 const elementsContainer = document.querySelector('.elements');
