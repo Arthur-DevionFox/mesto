5758a973f32b45dd541da62d922e4ca2126b4a12
Arthur-DevionFox <blackberserkfox@mail.ru>
2023-02-21 00:05:05 +0300

    закончить классы

--- JS/Card.js
+++ JS/Card.js
@@ -4,37 +4,52 @@ export class Card {
         this._name = cardItem.name;
         this._link = cardItem.link;
         this._openImagePopup = openImagePopup;
+
+        console.log(this);
     }
 
     _getTemplate() {
         return document
-            .querySelector('#element')
+            .querySelector(/*selector*/this._cardTemplate)
             .content.querySelector('.element')
             .cloneNode(true);
+        //
     }
 
-    _createCard() {
+    createCard() {
+        this._cardItem = this._getTemplate();
+        this._cardImage = this._cardItem.querySelector('.element__image');
+        this._likeBtn = this._cardItem.querySelector('.element__like');
+        this._deleteBtn = this._cardItem.querySelector('.element__delete');
+        this._paragraph = this._cardItem.querySelector('.element__paragraph');
 
+        this._cardImage.src = this._link;
+        this._cardImage.alt = this._name;
+        this._paragraph.textContent = this._name;
+
+        this._setEventListeners();
+
+        return this._cardItem;
     }
 
     _deleteCard() {
-
+        this._cardItem.remove();
     }
 
     _likeCard() {
-        this._cardTemplate.target.closest('.element').remove();
+        this._likeBtn.classList.toggle('element__like_active');
     }
 
     _setEventListeners() {
-        this._cardTemplate.querySelector('.element__like').addEventListener('click', () => {
+        this._likeBtn.addEventListener('click', () => {
             this._likeCard();
         })
 
-        this._cardTemplate.querySelector('.element__image').addEventListener('click', () => {
-            this._openImagePopup();
+        this._cardImage.addEventListener('click', () => {
+            this._openImagePopup(this._name, this._link);
         })
 
-        this._cardTemplate.querySelector('.element__delete').addEventListener('click', () => {
+        this._deleteBtn.addEventListener('click', () => {
             this._deleteCard();
         })
     }
---
+++ JS/constants.js
@@ -0,0 +1,25 @@
+export const popups = Array.from(document.querySelectorAll('.popup'))
+export const popupEdit = document.querySelector('#edit');
+export const popupAdd = document.querySelector('#add');
+export const popupImg = document.querySelector('#image');
+/*Кнопки изменения*/
+export const btnEdit = document.querySelector('.profile__edit-button');
+export const btnAdd = document.querySelector('.profile__add-button');
+/*Формы*/
+export const formEdit = document.querySelector('#form-edit');
+export const formAdd = document.querySelector('#form-add');
+/**/
+export const nameProf = document.querySelector('.profile__name')
+export const nameRes = document.querySelector('.popup__input_type_name');
+/**/
+export const profession = document.querySelector('.profile__profession');
+export const professionRes = document.querySelector('.popup__input_type_profession');
+/**/
+export const popupCardImage = document.querySelector('.popup__image');
+export const popupCardCaption = document.querySelector('.popup__undertaker');
+export const headInput = popupAdd.querySelector('.popup__input_type_head');
+export const urlInput = popupAdd.querySelector('.popup__input_type_url');
+
+/*Карточки*/
+export const cardsContainer = document.querySelector('.elements');
+
