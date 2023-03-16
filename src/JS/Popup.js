a62fdcb7684fd483f457a3197fade4a3defd7dab
Arthur-DevionFox <blackberserkfox@mail.ru>
2023-03-17 02:59:32 +0300

    Начать восьмую проектную

--- src/JS/Popup.js
+++ src/JS/Popup.js
@@ -1,20 +1,29 @@
 export default class Popup {
     constructor(popupSelector) {
-        this._popup = document.querySelector(popupSelector);
+        this._popup = popupSelector;
+        this._handleEscClose = this._handleEscClose.bind(this)
+        this._btnClose = document.querySelector('.popup__close')
     }
 
     open() {
-
+        this._popup.classList.add('popup_opened')
+        document.addEventListener('click', this._handleEscClose)
     }
     close() {
-
+        this._popup.classList.remove('popup_opened')
+        document.removeEventListener('click', this._handleEscClose)
     }
 
-    _handleEscClose() {
+    _handleEscClose(evt) {
+        evt.preventDefault();
+        if (evt.key === 'Escape') {
+            this._popup.classList.remove('popup_opened')
+
+        }
 
     }
 
     setEventListeners() {
-
+        this._btnClose.addEventListener('click', this.close)
     }
 }
--- src/JS/PopupWithImage.js
+++ src/JS/PopupWithImage.js
@@ -3,5 +3,10 @@ import Popup from "./Popup";
 export default class PopupWithImage extends Popup {
     constructor() {
         super(Popup);
+        this._popup = Array.from(document.querySelectorAll('.popup'))
+    }
+
+    open() {
+        super.open();
     }
 }
