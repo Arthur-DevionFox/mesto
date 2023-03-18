713c1985e52d0c7f14c28e29eed9350059077670
Arthur-DevionFox <blackberserkfox@mail.ru>
2023-03-19 02:32:47 +0300

    Почти закончить дл первого ревью

--- src/JS/PopupWithImage.js
+++ src/JS/PopupWithImage.js
@@ -1,12 +1,16 @@
 import Popup from "./Popup";
 
 export default class PopupWithImage extends Popup {
-    constructor() {
-        super(Popup);
-        this._popup = Array.from(document.querySelectorAll('.popup'))
+    constructor(popupSelector) {
+        super(popupSelector);
+        this._image = this._popup.querySelector('.popup__image')
+        this._undertaker = this._popup.querySelector('.popup__undertaker')
     }
 
-    open() {
+    open(item) {
         super.open();
+        this._image.src = item.link;
+        this._image.alt = item.name;
+        this._undertaker.textContent = item.name;
     }
 }
